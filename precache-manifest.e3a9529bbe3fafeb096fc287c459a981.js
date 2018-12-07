281ba906c8a41d7bb11de9da69c1864e4ea61966
Fernanda Santo <fgesanto@gmail.com>
2018-12-07 09:48:02 -0200

    Updates

---
+++ precache-manifest.e3a9529bbe3fafeb096fc287c459a981.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "15138ec5648c2bd9d529",
+    "url": "/projeto_final/static/js/runtime~main.15138ec5.js"
+  },
+  {
+    "revision": "217fccef2b30b3aac9f0",
+    "url": "/projeto_final/static/js/main.217fccef.chunk.js"
+  },
+  {
+    "revision": "8160c8142b0f7a49493c",
+    "url": "/projeto_final/static/js/1.8160c814.chunk.js"
+  },
+  {
+    "revision": "217fccef2b30b3aac9f0",
+    "url": "/projeto_final/static/css/main.8d681761.chunk.css"
+  },
+  {
+    "revision": "8160c8142b0f7a49493c",
+    "url": "/projeto_final/static/css/1.9ade15aa.chunk.css"
+  },
+  {
+    "revision": "843510aecd26c68e54c09f20d2de9450",
+    "url": "/projeto_final/index.html"
+  }
+];
